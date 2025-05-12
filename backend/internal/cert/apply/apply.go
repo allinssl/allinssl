b8246ba86e7cbeb7976945af8bb834c1e2f6721c
zhangchenhao <zch@bt.cn>
2025-05-12 20:19:21 +0800

    取消指定ns

--- backend/internal/cert/apply/apply.go
+++ backend/internal/cert/apply/apply.go
@@ -237,10 +237,10 @@ func Apply(cfg map[string]any, logger *public.Logger) (map[string]any, error) {
 			// 跳过预检查
 			return true, nil
 		}),
-		dns01.AddRecursiveNameservers([]string{
-			"8.8.8.8:53",
-			"1.1.1.1:53",
-		}),
+		// dns01.AddRecursiveNameservers([]string{
+		// 	"8.8.8.8:53",
+		// 	"1.1.1.1:53",
+		// }),
 	)
 	if err != nil {
 		return nil, err
