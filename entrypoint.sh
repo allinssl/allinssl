30dcbc3234df50d9e10220c7eec4ec659db6ba9c
mycoool <mycoool@aliyun.com>
2025-10-10 21:56:27 +0800

    【修复】避免DOCKER部署重置密码

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-if [ ! -f /www/allinssl/.initialized ]; then
+if [ ! -f /www/allinssl/data/.initialized ]; then
     echo ${ALLINSSL_USER:-allinssl} | /www/allinssl/allinssl 5
     echo ${ALLINSSL_URL:-allinssl} | /www/allinssl/allinssl 4
     echo ${ALLINSSL_PWD:-allinssldocker} | /www/allinssl/allinssl 6
     echo 8888 | /www/allinssl/allinssl 7
-    touch /www/allinssl/.initialized
+    touch /www/allinssl/data/.initialized
 fi
 /www/allinssl/allinssl 2
 exec /www/allinssl/allinssl start
